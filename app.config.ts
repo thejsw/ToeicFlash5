c43d55aeebb99521be95e4429b0042a5d65947c1
thejsw <jswtncapetnc@gmail.com>
2025-12-02 17:16:18 +0900

    git issue clear

---
+++ app.config.ts
@@ -0,0 +1,32 @@
+import type { ExpoConfig } from 'expo/config';
+
+const config: ExpoConfig = {
+  name: 'bolt-expo-nativewind',
+  slug: 'bolt-expo-nativewind',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/images/icon.png',
+  scheme: 'myapp',
+  userInterfaceStyle: 'automatic',
+  newArchEnabled: true,
+  ios: {
+    supportsTablet: true,
+  },
+  web: {
+    bundler: 'metro',
+    output: 'single',
+    favicon: './assets/images/favicon.png',
+  },
+  plugins: ['expo-router', 'expo-font', 'expo-web-browser'],
+  experiments: {
+    typedRoutes: true,
+  },
+  extra: {
+    supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
+    supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
+  },
+};
+
+export default config;
+
+
